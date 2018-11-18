87cc608a3d7734f1a4ecfa7144ad7bdebbca98fc
Ruchir Aggrawal <e0216119@u.nus.edu>
2018-11-18 12:09:22 -0500

    RawImageConversion.py: convert lists to numpy arrays RawImageConversion.py: correct m1,m2,m3,m4 and t expressions

--- Parkinsons_ML/main/RawImageConversion.py
+++ Parkinsons_ML/main/RawImageConversion.py
@@ -25,7 +25,6 @@ def convert_raw_images_to_csv():
 class RawImageConvert:
 
     def __init__(self):
-
         return
 
     def get_paths_to_raw_images(self):
@@ -38,29 +37,26 @@ class RawImageConvert:
 
         idx = image_data[image_data > (pow(2, 15))]
         for index in idx:
-            image_data[index] = image_data[index] - (pow(2,16))
+            image_data[index] = image_data[index] - (pow(2, 16))
 
         n = 256
-        m1 = []
-        m2 = []
-        m3 = []
-        m4 = []
+        m1 = np.empty([1, 1])
+        m2 = np.empty([1, 1])
+        m3 = np.empty([1, 1])
+        m4 = np.empty([1, 1])
         L = 4 * n * math.floor(len(image_data) / (4 * n))
 
-        for i in range(1, L):
-            m1 = m1.append(image_data[(i + 0 * n): (i + 1 * n - 1)])
-            m2 = m2.append(image_data[(i + 1 * n): (i + 2 * n - 1)])
-            m3 = m3.append(image_data[(i + 2 * n): (i + 3 * n - 1)])
-            m4 = m4.append(image_data[(i + 3 * n): (i + 4 * n - 1)])
+        print(L)
+        for i in range(0, 500):
+            print(i)
+            m1 = np.append(m1, image_data[(i + 0 * n): (i + 1 * n - 1)])
+            m2 = np.append(m2, image_data[(i + 1 * n): (i + 2 * n - 1)])
+            m3 = np.append(m3, image_data[(i + 2 * n): (i + 3 * n - 1)])
+            m4 = np.append(m4, image_data[(i + 3 * n): (i + 4 * n - 1)])
             i += n * 4
 
-        t = m1 / 44100
+        t = np.arange(len(m1)) / 44100
 
-        image_object = rawpy.imread(raw_image_path)
-        print(type(image_object))
-        print(image_object.raw_type())
-        image_data = image_object.postprocess()
-        image_object.close()
         return image_data
 
 
