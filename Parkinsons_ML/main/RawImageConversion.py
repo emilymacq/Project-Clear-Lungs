91238d170b227174ef493744fc795d3c5f0afb47
Ruchir Aggrawal <e0216119@u.nus.edu>
2018-11-18 10:46:13 -0500

    RawImageConversion.py: add file

---
+++ Parkinsons_ML/main/RawImageConversion.py
@@ -0,0 +1,23 @@
+
+# Import packages
+import os.path
+import rawpy
+
+# Import methods from other scripts
+
+
+# Constant Declarations
+path_to_raw_image_folder = os.path.abspath(os.path.join(yourpath, os.pardir))
+
+
+class RawImageConvert:
+
+    def __init__(self):
+
+        return
+
+    def read_raw_image(self):
+
+
+
+
